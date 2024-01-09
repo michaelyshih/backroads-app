f5caafa78bced3aad78dcc4e9210df27c33345fa
shihyulung <shihyulung@yahoo.com.tw>
2024-01-08 17:24:47 -0800

    remove readmore

--- src/components/About.js
+++ src/components/About.js
@@ -25,9 +25,9 @@ const About = () => {
             quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
             unde dolor?
           </p>
-          <a href="#" className="btn">
+          {/* <a href="#" className="btn">
             read more
-          </a>
+          </a> */}
         </article>
       </div>
     </section>
